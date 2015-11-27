b46d30e9a41c55b730d0ef76fec2fa5ba3a3fdc3
Pavel Krajcevski <Krajcevski@gmail.com>
2015-11-27 16:13:13 -0500

    Add sphere creation test

--- src/shape/sphere.rs
+++ src/shape/sphere.rs
@@ -6,6 +6,7 @@ use transform::transform::Transform;
 use utils::Degrees;
 use utils::Clamp;
 
+#[derive(Debug, PartialEq, Clone)]
 pub struct Sphere {
     shape: Shape,
     radius: f32,
@@ -22,8 +23,9 @@ impl Sphere {
         debug_assert!(rad > 0f32);
         let zmin = z0.min(z1).clamp(-rad, rad);
         let zmax = z0.max(z1).clamp(-rad, rad);
-        let thetamin = (zmin / rad).clamp(-1f32, 1f32).acos();
-        let thetamax = (zmax / rad).clamp(-1f32, 1f32).acos();
+        let thetamin = (zmin / rad).acos();
+        let thetamax = (zmax / rad).acos();
+        println!("{:?}", (thetamin, zmin / rad, (-1f32).acos()));
         Sphere {
             shape: Shape::new(o2w, w2o, ro),
             radius: rad,
@@ -31,7 +33,7 @@ impl Sphere {
             z_max: zmax,
             theta_min: thetamin,
             theta_max: thetamax,
-            phi_max: pm.clamp(0f32, 360f32).as_radians()
+            phi_max: pm.clamp(0.0, 360.0).as_radians()
         }
     }
 }
@@ -42,5 +44,30 @@ impl IsShape for Sphere {
         BBox::new_with(
             Point::new_with(-self.radius, -self.radius, self.z_min),
             Point::new_with(self.radius, self.radius, self.z_max))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use shape::shape::Shape;
+    use transform::transform::Transform;
+
+    #[test]
+    fn it_can_be_created() {
+        assert_eq!(Sphere::new(Transform::new(), Transform::new(),
+                               false, 1.0, -1.0, 1.0, 360.0),
+                   Sphere {
+                       shape: Shape::new(Transform::new(), Transform::new(), false),
+                       radius: 1.0,
+                       z_min: -1.0,
+                       z_max: 1.0,
+                       // This is PI but due to floating point precision errors,
+                       // we want to compare against whatever approximated version
+                       // of PI we actually produce...
+                       theta_min: (-1f32).acos(),
+                       theta_max: 0.0,
+                       phi_max: ::std::f32::consts::PI * 2.0
+                   });
     }
 }
