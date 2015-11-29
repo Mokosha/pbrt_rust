7c2d2630d8bfa1b5a54f9ac6b78d31267f589d6e
Pavel Krajcevski <Krajcevski@gmail.com>
2015-11-28 21:11:20 -0500

    Finish section 3.2.3
    So far we're on our way to doing whole ray-sphere intersection
    but it still needs some work. In particular, the tests should do
    a better job of figuring out where rays intersect the sphere and
    test for that, too.

--- src/shape/sphere.rs
+++ src/shape/sphere.rs
@@ -1,8 +1,14 @@
 use bbox::BBox;
+use diff_geom::DifferentialGeometry;
 use geometry::point::Point;
+use geometry::vector::Dot;
+use geometry::vector::Vector;
+use ray::Ray;
 use shape::shape::Shape;
+use shape::shape::ShapeIntersection;
 use shape::shape::IsShape;
 use transform::transform::Transform;
+use transform::transform::ApplyTransform;
 use utils::Degrees;
 use utils::Clamp;
 
@@ -25,7 +31,6 @@ impl Sphere {
         let zmax = z0.max(z1).clamp(-rad, rad);
         let thetamin = (zmin / rad).acos();
         let thetamax = (zmax / rad).acos();
-        println!("{:?}", (thetamin, zmin / rad, (-1f32).acos()));
         Sphere {
             shape: Shape::new(o2w, w2o, ro),
             radius: rad,
@@ -45,12 +50,66 @@ impl IsShape for Sphere {
             Point::new_with(-self.radius, -self.radius, self.z_min),
             Point::new_with(self.radius, self.radius, self.z_max))
     }
+
+    fn intersect(&self, r: &Ray) -> Option<ShapeIntersection> {
+        let phi : f32 = 0.0;
+        let phit : Point = Point::new();
+
+        // Transform ray to object space
+        let ray = self.get_shape().world2object.t(r);
+
+        // Compute quadratic sphere coefficients
+        let a = ray.d.length_squared();
+        let b = 2.0 * ray.d.dot(&Vector::from(ray.o.clone()));
+        let c = Vector::from(ray.o.clone()).length_squared() -
+            self.radius * self.radius;
+
+        // Solve quadratic equation for t values
+        let (t0, t1) = {
+            match ::utils::quadratic(a, b, c) {
+                None => return None,
+                Some((x, y)) => (x, y)
+            }
+        };
+
+        // Compute intersection distance along ray
+        if t0 > ray.maxt || t1 < ray.mint {
+            return None
+        }
+
+        let mut t_hit = t0;
+        if t0 < ray.mint {
+            t_hit = t1;
+            if t_hit > ray.maxt {
+                return None;
+            }
+        }
+
+        // Compute sphere hit position and phi
+        // Test sphere intersection against clipping parameters
+        // Find parametric representation of sphere hit
+        // Initialize DifferentialGeometry from parametric information
+        let dg : DifferentialGeometry = DifferentialGeometry::new();
+
+        // update t_hit for quadric intersection
+        let t_hit: f32 = 0.0;
+
+        // compute ray_epsilon for quadric intersection
+        let ray_epsilon : f32 = 0.0;
+
+        Some(ShapeIntersection::new(t_hit, ray_epsilon, dg))
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use geometry::vector::Vector;
+    use geometry::point::Point;
+    use ray::Ray;
     use shape::shape::Shape;
+    use shape::shape::IsShape;
+    use shape::shape::ShapeIntersection;
     use transform::transform::Transform;
 
     #[test]
@@ -69,5 +128,47 @@ mod tests {
                        theta_max: 0.0,
                        phi_max: ::std::f32::consts::PI * 2.0
                    });
+    }
+
+    #[test]
+    fn it_can_be_intersected() {
+        let xf = Transform::translate(&Vector::new_with(1.0, 2.0, 1.0));
+        let xf_inv = xf.inverse();
+        let s = Sphere::new(xf, xf_inv, false, 1.0, -1.0, 1.0, 360.0);
+
+        // A full sphere should be able to be intersected at will...
+        assert!(s.can_intersect());
+
+        // !FIXME! We need to actually investigate that our ray hits
+        // where we think it does rather than it just hits at all...
+
+        assert!(s.intersect(
+            &Ray::new_with(Point::new_with(0.0, 0.0, 0.0),
+                           Vector::new_with(1.0, 1.5, 1.0), 0.0)).is_some());
+        assert!(s.intersect(
+            &Ray::new_with(Point::new_with(0.0, 0.0, 0.0),
+                           Vector::new_with(1.0, 1.0, 1.0), 0.0)).is_some());
+        assert_eq!(None, s.intersect(
+            &Ray::new_with(Point::new_with(0.0, 0.0, 0.0),
+                           Vector::new_with(1.0, 0.5, 1.0), 0.0)));
+        assert!(s.intersect(
+            &Ray::new_with(Point::new_with(0.0, 0.0, 0.0),
+                           Vector::new_with(0.0, 2.0, 1.0), 0.0)).is_some());
+        assert!(s.intersect(
+            &Ray::new_with(Point::new_with(0.0, 0.0, 0.0),
+                           Vector::new_with(1.0, 2.0, 0.0), 0.0)).is_some());
+
+        // A non-full sphere should also be able to be intersected...
+        let xf2 = Transform::translate(&Vector::new_with(0.0, -3.0, 0.0))
+            * Transform::scale(2.0, 2.0, 2.0);
+        let xf2_inv = xf2.inverse();
+        let s2 = Sphere::new(xf2, xf2_inv, false, 0.75, 0.5, 0.75, 180.0);
+        assert!(s2.can_intersect());
+        assert!(s2.intersect(
+            &Ray::new_with(Point::new(),
+                           Vector::new_with(0.0, -1.0, 0.0), 0.0)).is_some());
+        assert_eq!(None, s2.intersect(
+            &Ray::new_with(Point::new_with(0.0, -4.0, 10.0),
+                           Vector::new_with(0.0, 0.0, -1.0), 0.0)));
     }
 }
