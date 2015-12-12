b0bff8709822fc8a04af60124f7b99911832b9f3
Pavel Krajcevski <Krajcevski@gmail.com>
2015-12-12 16:19:31 -0500

    Add object and world bounds for meshes

--- src/shape/mesh.rs
+++ src/shape/mesh.rs
@@ -37,10 +37,28 @@ impl Mesh {
     }
 }
 
+impl IsShape for Mesh {
+    fn get_shape<'a>(&'a self) -> &'a Shape { &(self.shape) }
+    fn object_bound(&self) -> BBox {
+        let w2o = &(self.get_shape().world2object);
+        self.p.iter().fold(BBox::new(), |b, p| b.unioned_with(&(w2o.t(p))))
+    }
+
+    fn world_bound(&self) -> BBox {
+        self.p.iter().fold(BBox::new(), |b, p| b.unioned_with(p))
+    }
+
+    // Cannot intersect meshes directly.
+    fn can_intersect(&self) -> bool { false }
+
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use bbox::BBox;
     use geometry::point::Point;
+    use shape::shape::IsShape;
     use transform::transform::Transform;
 
     use texture::white_float_tex;
@@ -78,5 +96,40 @@ mod tests {
             |(p, q)| {
                 (p - q).length_squared() < 1e-6
             }));
+    }
+
+    #[test]
+    fn it_has_object_space_bounds() {
+        let xf = Transform::rotate_y(90.0);
+        let mesh = Mesh::new(xf.clone(), xf.inverse(), false,
+                             &tet_tris, &tet_pts, None, None, None,
+                             ::std::rc::Rc::new(white_float_tex()));
+        let expected = BBox::new_with(Point::new(),
+                                      Point::new_with(1.0, 1.0, 1.0));
+
+        assert_eq!(mesh.object_bound(), expected);
+        assert_eq!(Mesh::new(Transform::new(), Transform::new(), false,
+                             &tet_tris, &tet_pts, None, None, None,
+                             ::std::rc::Rc::new(white_float_tex())).object_bound(),
+                   expected);
+    }
+
+    #[test]
+    fn it_has_world_space_bounds() {
+        let xf = Transform::rotate_y(90.0);
+        let mesh = Mesh::new(xf.clone(), xf.inverse(), false,
+                             &tet_tris, &tet_pts, None, None, None,
+                             ::std::rc::Rc::new(white_float_tex()));
+
+        let expected = BBox::new_with(Point::new_with(0.0, 0.0, -1.0),
+                                      Point::new_with(1.0, 1.0, 0.0));
+
+
+        assert!((mesh.world_bound().p_min - expected.p_min).length_squared() < 1e-6);
+        assert!((mesh.world_bound().p_max - expected.p_max).length_squared() < 1e-6);
+        assert_eq!(Mesh::new(Transform::new(), Transform::new(), false,
+                             &tet_tris, &tet_pts, None, None, None,
+                             ::std::rc::Rc::new(white_float_tex())).world_bound(),
+                   BBox::new_with(Point::new(), Point::new_with(1.0, 1.0, 1.0)));
     }
 }
