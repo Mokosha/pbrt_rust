966e2262b6aea3d74310e3071e5a44f5406d0f0b
Pavel Krajcevski <Krajcevski@gmail.com>
2016-01-11 23:33:17 -0500

    Flesh out intersection struct members a bit more

--- src/intersection.rs
+++ src/intersection.rs
@@ -1,19 +1,39 @@
 use bsdf::BSDF;
+use diff_geom::DifferentialGeometry;
 use geometry::vector::Vector;
+use primitive::Primitive;
 use ray::Ray;
 use spectrum::Spectrum;
+use transform::transform::Transform;
 
 #[derive(Debug, Clone)]
 pub struct Intersection<'a> {
+    pub dg: DifferentialGeometry<'a>,
+    pub primitive: Option<&'a Primitive>, // !FIXME! This shouldn't be an option.
+    pub world_to_object: Transform,
+    pub object_to_world: Transform,
+    pub shape_id: usize,
+    pub primitive_id: usize,
     pub ray_epsilon: f32,
     bsdf: BSDF<'a>
 }
 
 impl<'a> Intersection<'a> {
-    pub fn new() -> Intersection<'a> { Intersection {
-        ray_epsilon: 0f32,
-        bsdf: BSDF::new()
-    } }
+    // !FIXME! We should get rid of this when we generate intersections
+    // based on actually intersecting things in the future...
+    pub fn new() -> Intersection<'a> {
+        Intersection {
+            dg: DifferentialGeometry::new(),
+            primitive: None,
+            world_to_object: Transform::new(),
+            object_to_world: Transform::new(),
+            shape_id: 0,
+            primitive_id: 0,
+            ray_epsilon: 0f32,
+            bsdf: BSDF::new()
+        }
+    }
+
     pub fn get_bsdf(&self) -> &BSDF<'a> { &self.bsdf }
     pub fn le(&self, dir: &Vector) -> Spectrum { Spectrum::from_value(0f32) }
 }
