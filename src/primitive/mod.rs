f9f84dec53590a61d9e45a204c866b835781d54a
Pavel Krajcevski <Krajcevski@gmail.com>
2016-01-14 01:23:40 -0500

    Add transformed primitives to list of primitives

--- src/primitive/mod.rs
+++ src/primitive/mod.rs
@@ -12,11 +12,14 @@ use intersection::Intersection;
 use material::Material;
 use ray::Ray;
 use shape::Shape;
+use transform::animated::AnimatedTransform;
 use transform::transform::Transform;
 
 use primitive::geometric::GeometricPrimitive;
+use primitive::transformed::TransformedPrimitive;
 
 use std::sync::atomic::AtomicUsize;
+use std::sync::Arc;
 
 #[derive(Clone, Debug)]
 pub struct PrimitiveBase {
@@ -59,7 +62,8 @@ pub trait FullyRefinable : Refinable<Self>+Sized {
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum Primitive {
-    Geometric(GeometricPrimitive)
+    Geometric(GeometricPrimitive),
+    Transformed(TransformedPrimitive)
 }
 
 impl Primitive {
@@ -67,9 +71,14 @@ impl Primitive {
         Primitive::Geometric(GeometricPrimitive::new(s, Material))
     }
 
+    pub fn transformed(p: Arc<Primitive>, xf: AnimatedTransform) -> Primitive {
+        Primitive::Transformed(TransformedPrimitive::new(p, xf))
+    }
+
     pub fn area_light(&self) -> Option<AreaLight> {
         match self {
-            &Primitive::Geometric(ref p) => p.area_light().clone()
+            &Primitive::Geometric(ref p) => p.area_light().clone(),
+            _ => panic!("Only geometric primitives may have area lights")
         }
     }
 
@@ -77,6 +86,7 @@ impl Primitive {
                         o2w: &Transform) -> Option<BSDF<'a>> {
         match self {
             &Primitive::Geometric(ref p) => p.get_bsdf(dg, o2w),
+            _ => panic!("Only geometric primitives may have bsdfs")
         }
     }
 
@@ -84,6 +94,7 @@ impl Primitive {
                           o2w: &Transform) -> Option<BSSRDF<'a>> {
         match self {
             &Primitive::Geometric(ref p) => p.get_bssrdf(dg, o2w),
+            _ => panic!("Only geometric primitives may have bssrdfs")
         }
     }
 }
@@ -91,28 +102,29 @@ impl Primitive {
 impl HasBounds for Primitive {
     fn world_bound(&self) -> BBox {
         match self {
-            &Primitive::Geometric(ref prim) => { prim.world_bound() }
+            &Primitive::Geometric(ref prim) => prim.world_bound(),
+            &Primitive::Transformed(ref p) => p.world_bound()
         }
     }
 }
 
 impl<'a> Intersectable<'a> for Primitive {
     fn intersect(&'a self, ray : &Ray) -> Option<Intersection<'a>> {
-        let mut isect = match self {
-            &Primitive::Geometric(ref prim) => { prim.intersect(ray) }
-        };
-
-        isect.as_mut().map(|t| {
-            t.primitive = Some(self)
-        });
-        isect
+        match self {
+            &Primitive::Geometric(ref prim) => {
+                prim.intersect(ray).and_then(|mut isect| {
+                    isect.primitive = Some(self);
+                    Some(isect)
+                })
+            },
+            &Primitive::Transformed(ref prim) => prim.intersect(ray)
+        }
     }
 
     fn intersect_p(&'a self, ray : &Ray) -> bool {
         match self {
-            &Primitive::Geometric(ref prim) => {
-                prim.intersect_p(ray)
-            }
+            &Primitive::Geometric(ref prim) => prim.intersect_p(ray),
+            &Primitive::Transformed(ref prim) => prim.intersect_p(ray)
         }
     }
 }
@@ -122,12 +134,18 @@ impl Refinable for Primitive {
         match self {
             Primitive::Geometric(p) =>
                 p.refine().iter().cloned().map(Primitive::Geometric).collect(),
+            Primitive::Transformed(_) =>
+                panic!("Transformed primitive should already be refined!")
         }
     }
 
     fn is_refined(&self) -> bool {
         match self {
-            &Primitive::Geometric(ref p) => p.is_refined()
+            &Primitive::Geometric(ref p) => p.is_refined(),
+            &Primitive::Transformed(ref p) => {
+                assert!(p.primitive().is_refined());
+                true
+            }
         }
     }
 }
