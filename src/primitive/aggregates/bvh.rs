ad4903d801df4a381edde551a5fba91a8fd064f6
Pavel Krajcevski <Krajcevski@gmail.com>
2016-01-19 23:47:24 -0500

    A small amount of progress on BVH trees

--- src/primitive/aggregates/bvh.rs
+++ src/primitive/aggregates/bvh.rs
@@ -1,3 +1,6 @@
+use bbox::BBox;
+use bbox::HasBounds;
+use geometry::point::Point;
 use primitive::Primitive;
 use primitive::FullyRefinable;
 
@@ -8,11 +11,41 @@ enum SplitMethod {
     SAH
 }
 
+enum BVHNode {
+    Empty,
+    Leaf {
+        bounds: BBox,
+        first_prim_offset: usize,
+        num_primitives: usize
+    },
+    Inner {
+        bounds: BBox,
+        children: [Box<BVHNode>; 2],
+        split_axis: usize
+    }
+}
+
+struct BVHPrimitiveInfo {
+    primitive_number: usize,
+    centroid: Point,
+    bounds: BBox,
+}
+
+impl BVHPrimitiveInfo {
+    fn new(pn: usize, bnds: BBox) -> BVHPrimitiveInfo {
+        BVHPrimitiveInfo {
+            primitive_number: pn,
+            centroid: (&bnds.p_min + &bnds.p_max) * 0.5,
+            bounds: bnds
+        }
+    }
+}
+
 #[derive(Clone, Debug)]
 pub struct BVHAccel {
     max_prims_in_node: usize,
     split_method: SplitMethod,
-    primitives: Vec<Primitive>
+    primitives: Vec<Primitive>,
 }
 
 impl BVHAccel {
@@ -22,18 +55,27 @@ impl BVHAccel {
             ps
         });
 
+        let split_method = match sm {
+            "sah" => SplitMethod::SAH,
+            "middle" => SplitMethod::Middle,
+            "equal" => SplitMethod::EqualCounts,
+            _ => {
+                println!("Warning: BVH split method {} unknown. Using \"SAH\"", sm);
+                SplitMethod::SAH
+            }
+        };
+
+        let build_data: Vec<_> = prims.iter().enumerate().map(|(i, p)| {
+            let bbox = p.world_bound();
+            BVHPrimitiveInfo::new(i, bbox)
+        }).collect();
+
+        // let (tree, ordered_prims) = recursive_build(build_data, prims);
+
         BVHAccel {
             max_prims_in_node: ::std::cmp::min(mp, 255),
-            split_method: match sm {
-                "sah" => SplitMethod::SAH,
-                "middle" => SplitMethod::Middle,
-                "equal" => SplitMethod::EqualCounts,
-                _ => {
-                    println!("Warning: BVH split method {} unknown. Using \"SAH\"", sm);
-                    SplitMethod::SAH
-                },
-            },
-            primitives: prims
+            split_method: split_method,
+            primitives: prims,
         }
     }
 }
