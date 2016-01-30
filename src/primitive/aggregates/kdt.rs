1e74e3d34edbcb25e61a8eb15bbaa3fd60d61be9
Pavel Krajcevski <Krajcevski@gmail.com>
2016-01-29 23:50:19 -0500

    Add utility class for creating kdtrees

--- src/primitive/aggregates/kdt.rs
+++ src/primitive/aggregates/kdt.rs
@@ -1,5 +1,106 @@
 use primitive::Primitive;
 
+#[derive(Clone, Copy, PartialEq, Debug)]
+enum SplitAxis { X, Y, Z }
+
+// !SPEED! Rust uses tagged unions to construct enum values. That
+// means that each of these values will be 8 bytes larger than they
+// need to be to hold the tag, reaching 16 bytes instead of eight.
+// We can get tricky with data layout to make the smaller to improve
+// cache performance. (See section 4.5.1)
+#[derive(Clone, PartialEq, Debug)]
+enum KDAccelNode {
+    InteriorX {
+        split: f32,
+        above_child: usize
+    },
+    InteriorY {
+        split: f32,
+        above_child: usize
+    },
+    InteriorZ {
+        split: f32,
+        above_child: usize
+    },
+    Leaf(Vec<usize>)
+}
+
+impl KDAccelNode {
+    fn leaf(prim_nums: Vec<usize>) -> KDAccelNode {
+        KDAccelNode::Leaf(prim_nums)
+    }
+
+    fn interior(axis: SplitAxis, ac: usize, s: f32) -> KDAccelNode {
+        match axis {
+            SplitAxis::X => KDAccelNode::InteriorX { split: s, above_child: ac },
+            SplitAxis::Y => KDAccelNode::InteriorY { split: s, above_child: ac },
+            SplitAxis::Z => KDAccelNode::InteriorZ { split: s, above_child: ac },
+        }
+    }
+
+    fn split_pos(&self) -> f32 {
+        match self {
+            &KDAccelNode::InteriorX { split, .. } => split,
+            &KDAccelNode::InteriorY { split, .. } => split,
+            &KDAccelNode::InteriorZ { split, .. } => split,
+            _ => panic!("Leaf nodes have no split node...")
+        }
+    }
+
+    fn split_axis(&self) -> SplitAxis {
+        match self {
+            &KDAccelNode::InteriorX {..} => SplitAxis::X,
+            &KDAccelNode::InteriorY {..} => SplitAxis::Y,
+            &KDAccelNode::InteriorZ {..} => SplitAxis::Z,
+            _ => panic!("Leaf nodes have no split axis...")
+        }
+    }
+
+    fn above_child(&self) -> usize {
+        match self {
+            &KDAccelNode::InteriorX { above_child, .. } => above_child,
+            &KDAccelNode::InteriorY { above_child, .. } => above_child,
+            &KDAccelNode::InteriorZ { above_child, .. } => above_child,
+            _ => panic!("Leaf nodes have no above_child...")
+        }
+    }
+
+    fn is_leaf(&self) -> bool {
+        match self {
+            &KDAccelNode::Leaf(_) => true,
+            _ => false
+        }
+    }
+
+    fn num_prims(&self) -> usize {
+        match self {
+            &KDAccelNode::Leaf(ref prim_ids) => prim_ids.len(),
+            _ => panic!("Interior nodes don't know how many primitives they have...")
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
 pub struct KDTreeAccelerator {
+    isect_cost: i32,
+    traversal_cost: i32,
+    max_prims: usize,
+    max_depth: usize,
+    empty_bonus: f32,
     primitives: Vec<Primitive>
+}
+
+impl KDTreeAccelerator {
+    fn new(prims: Vec<Primitive>, icost: i32, tcost: i32, ebonus: f32,
+           maxp: usize, maxd: usize) -> KDTreeAccelerator {
+        // Build kd-tree for accelerator
+        KDTreeAccelerator {
+            isect_cost: icost,
+            traversal_cost: tcost,
+            max_prims: maxp,
+            max_depth: maxd,
+            empty_bonus: ebonus,
+            primitives: prims
+        }
+    }
 }
