b24bf767cbada14306839b9f213b6da68eea65f5
Pavel Krajcevski <Krajcevski@gmail.com>
2016-02-23 22:45:30 -0500

    Move subwindow to samplerbase

--- src/sampler/mod.rs
+++ src/sampler/mod.rs
@@ -9,7 +9,7 @@ use spectrum::Spectrum;
 use utils::Lerp;
 
 #[derive(Debug, Clone)]
-pub struct SamplerBase {
+struct SamplerBase {
     x_pixel_start: i32,
     x_pixel_end: i32,
     y_pixel_start: i32,
@@ -32,6 +32,37 @@ impl SamplerBase {
             shutter_close: sclose
         }
     }
+
+    pub fn compute_sub_window(&self, num: usize, count: usize) -> (i32, i32, i32, i32) {
+        // Determine how many tiles to use in each dimension nx and ny
+        let dx = (self.x_pixel_end - self.x_pixel_start) as usize;
+        let dy = (self.y_pixel_end - self.y_pixel_start) as usize;
+
+        let mut nx = count;
+        let mut ny = 1;
+        while (nx % 2) == 0 && 2*dx*ny < dy*nx {
+            nx /= 2;
+            ny *= 2;
+        }
+
+        // Compute x and y pixel sample range for sub window
+        let xo = num % nx;
+        let yo = num / nx;
+
+        let tx0 = (xo as f32) / (nx as f32);
+        let tx1 = ((xo + 1) as f32) / (nx as f32);
+
+        let ty0 = (yo as f32) / (ny as f32);
+        let ty1 = ((yo + 1) as f32) / (ny as f32);
+
+        let new_x_start = (self.x_pixel_start as f32).lerp(&(self.x_pixel_end as f32), tx0) as i32;
+        let new_x_end = (self.x_pixel_start as f32).lerp(&(self.x_pixel_end as f32), tx1) as i32;
+        let new_y_start = (self.y_pixel_start as f32).lerp(&(self.y_pixel_end as f32), tx0) as i32;
+        let new_y_end = (self.y_pixel_start as f32).lerp(&(self.y_pixel_end as f32), tx1) as i32;
+
+        (new_x_start, new_x_end, new_y_start, new_y_end)
+    }
+
 }
 
 pub struct Sampler {
@@ -69,36 +100,6 @@ impl Sampler {
                           ls: &Vec<Spectrum>,
                           isects: &Vec<Intersection>,
                           sample_count: usize) -> bool { true }
-
-    pub fn compute_sub_window(&self, num: usize, count: usize) -> (i32, i32, i32, i32) {
-        // Determine how many tiles to use in each dimension nx and ny
-        let dx = (self.base().x_pixel_end - self.base().x_pixel_start) as usize;
-        let dy = (self.base().y_pixel_end - self.base().y_pixel_start) as usize;
-
-        let mut nx = count;
-        let mut ny = 1;
-        while (nx % 2) == 0 && 2*dx*ny < dy*nx {
-            nx /= 2;
-            ny *= 2;
-        }
-
-        // Compute x and y pixel sample range for sub window
-        let xo = num % nx;
-        let yo = num / nx;
-
-        let tx0 = (xo as f32) / (nx as f32);
-        let tx1 = ((xo + 1) as f32) / (nx as f32);
-
-        let ty0 = (yo as f32) / (ny as f32);
-        let ty1 = ((yo + 1) as f32) / (ny as f32);
-
-        let new_x_start = (self.base().x_pixel_start as f32).lerp(&(self.base().x_pixel_end as f32), tx0) as i32;
-        let new_x_end = (self.base().x_pixel_start as f32).lerp(&(self.base().x_pixel_end as f32), tx1) as i32;
-        let new_y_start = (self.base().y_pixel_start as f32).lerp(&(self.base().y_pixel_end as f32), tx0) as i32;
-        let new_y_end = (self.base().y_pixel_start as f32).lerp(&(self.base().y_pixel_end as f32), tx1) as i32;
-
-        (new_x_start, new_x_end, new_y_start, new_y_end)
-    }
 }
 
 #[cfg(test)]
@@ -113,7 +114,7 @@ mod tests {
 
     #[ignore]
     #[test]
-    fn it_can_tile_windows() {
+    fn its_base_can_tile_windows() {
         unimplemented!()
     }
 }
