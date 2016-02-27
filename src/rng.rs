3b07c0d5807ebad67e1a0ff2d73754ac0631dc2a
Pavel Krajcevski <Krajcevski@gmail.com>
2016-02-26 22:39:42 -0500

    Use [T]::swap in RNG

--- src/rng.rs
+++ src/rng.rs
@@ -11,16 +11,14 @@ impl RNG {
     }
 }
 
-pub fn shuffle<T: Copy>(v: &mut [T], dims: usize, rng: &mut RNG) {
+pub fn shuffle<T>(v: &mut [T], dims: usize, rng: &mut RNG) {
     let count = v.len() / dims;
     assert!(count * dims == v.len());
 
     for i in 0..count {
         let other = i + rng.random_uint() % (count - i);
         for j in 0..dims {
-            let t = v[dims*i+j];
-            v[dims*i+j] = v[dims*other+j];
-            v[dims*other+j] = t;
+            v.swap(dims*i+j, dims*other+j);
         }
     }
 }
