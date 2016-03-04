c0529a24d5e510f1b2e23c53a4b56d4a4b692f40
Pavel Krajcevski <Krajcevski@gmail.com>
2016-03-04 00:20:50 -0500

    Add 02 samples

--- src/sampler/utils.rs
+++ src/sampler/utils.rs
@@ -11,3 +11,22 @@ fn van_der_corput(_n: u32, scramble: u32) -> f32 {
     n ^= scramble;
     ((((n >> 8) & 0xffffff) as f64) / ((1 << 24) as f64)) as f32
 }
+
+fn sobol2(_n: u32, scramble: u32) -> f32 {
+    let mut s = scramble;
+    let mut n = _n;
+    let mut v: u32 = 1 << 31;
+    while n != 0 {
+        if (n & 0x1) == 0 {
+            s ^= v;
+        }
+        v ^= (v >> 1);
+        n >>= 1;
+    }
+
+    ((((s >> 8) & 0xFFFFFF) as f64) / ((1 << 24) as f64)) as f32
+}
+
+pub fn sample02(n: u32, scramble: [u32; 2]) -> (f32, f32) {
+    (van_der_corput(n, scramble[0]), sobol2(n, scramble[1]))
+}
