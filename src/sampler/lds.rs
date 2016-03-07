2bf1a598cb597241d519c834b91b8eaee9cefe35
Pavel Krajcevski <Krajcevski@gmail.com>
2016-03-06 21:16:02 -0500

    Start work on the actual method to generate samples for LDSampler

--- src/sampler/lds.rs
+++ src/sampler/lds.rs
@@ -1,3 +1,5 @@
+use rng::RNG;
+use sampler::sample::Sample;
 use sampler::base::SamplerBase;
 
 pub struct LDSampler {
@@ -5,6 +7,29 @@ pub struct LDSampler {
     x_pos: i32,
     y_pos: i32,
     sample_buf: Vec<f32>
+}
+
+fn ld_pixel_sample_floats_needed(sample: &Sample, num_pixel_samples: usize) -> usize {
+    let mut n = 5;  // 2 image, 2 lens, 1 time
+    for &ns in sample.n1D.iter() {
+        n += ns;
+    }
+
+    for &ns in sample.n2D.iter() {
+        n += 2 * ns;
+    }
+
+    n * num_pixel_samples
+}
+
+fn ld_pixel_sample(x_pos: i32, y_pos: i32, shutter_open: f32, shutter_close: f32,
+                   num_pixel_samples: usize, sample: &mut Vec<Sample>,
+                   buf: &mut [f32], rng: &mut RNG) {
+    // Prepare temporary array pointers for low-discrepancy camera samples
+    // Prepare temporary array pointers for low-discrepancy integrator samples
+    // Generate low-discrepancy pixel samples
+    // Initialize samples with computed sample values
+    unimplemented!()
 }
 
 impl LDSampler {
@@ -44,5 +69,27 @@ impl LDSampler {
                                 self.base.shutter_open,
                                 self.base.shutter_close))
         }
+    }
+
+    pub fn get_more_samples(&mut self, samples: &mut Vec<Sample>,
+                            rng: &mut RNG) -> usize {
+        assert!(samples.len() >= 1);
+        if self.y_pos == self.base.y_pixel_end { return 0 }
+
+        let ns = ld_pixel_sample_floats_needed(samples.get(0).unwrap(),
+                                               self.base.samples_per_pixel);
+        self.sample_buf.resize(ns, 0f32);
+
+        ld_pixel_sample(self.x_pos, self.y_pos, self.base.shutter_open,
+                        self.base.shutter_close, self.base.samples_per_pixel,
+                        samples, &mut self.sample_buf, rng);
+
+        self.x_pos += 1;
+        if self.x_pos == self.base.x_pixel_end {
+            self.x_pos = self.base.x_pixel_start;
+            self.y_pos += 1;
+        }
+
+        self.base.samples_per_pixel
     }
 }
