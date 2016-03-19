fc813f2cd0a883a413f4f9e0fa3b2435f9ca4ffc
Pavel Krajcevski <Krajcevski@gmail.com>
2016-03-18 22:39:20 -0400

    Add triangle filter

--- src/filter.rs
+++ src/filter.rs
@@ -17,7 +17,8 @@ impl FilterBase {
 }
 
 pub enum FilterType {
-    Mean    // Also known as a box filter
+    Mean,    // Also known as a box filter
+    Triangle,
 }
 
 pub struct Filter {
@@ -33,9 +34,21 @@ impl Filter {
         }
     }
 
+    pub fn triangle(xw: f32, yw: f32) -> Filter {
+        Filter {
+            base: FilterBase::new(xw, yw),
+            ty: FilterType::Triangle
+        }
+    }
+
     pub fn evaluate(&self, x: f32, y: f32) -> f32 {
         match &self.ty {
-            &FilterType::Mean => 1.0
+            &FilterType::Mean => 1.0,
+            &FilterType::Triangle => {
+                let dx = (self.x_width() - x.abs()) * self.inv_x_width();
+                let dy = (self.y_width() - y.abs()) * self.inv_y_width();
+                dx.max(0.0) * dy.max(0.0)
+            }
         }
     }
 
@@ -61,5 +74,21 @@ mod tests {
                 assert_eq!(filter.evaluate(x, y), 1.0);
             }
         }
+    }
+
+    #[test]
+    fn it_can_evaluate_triangle_filters() {
+        let filter = Filter::triangle(2.0, 2.0);
+
+        assert_eq!(filter.evaluate(1.0, 0.0), 0.5);
+        assert_eq!(filter.evaluate(0.0, 0.0), 1.0);
+        assert_eq!(filter.evaluate(20.0, 0.0), 0.0);
+        assert_eq!(filter.evaluate(-20.0, 0.0), 0.0);
+        assert_eq!(filter.evaluate(0.0, 20.0), 0.0);
+        assert_eq!(filter.evaluate(0.0, -20.0), 0.0);
+        assert_eq!(filter.evaluate(1.0, 1.0), 0.25);
+        assert_eq!(filter.evaluate(0.5, 0.5), 0.5625);
+        assert_eq!(filter.evaluate(2.0, 0.0), 0.0);
+        assert_eq!(filter.evaluate(0.0, -2.0), 0.0);
     }
 }
