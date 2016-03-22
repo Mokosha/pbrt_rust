7641183c2bddc2147733a6310cbf16e921f0cd4e
Pavel Krajcevski <Krajcevski@gmail.com>
2016-03-22 00:09:57 -0400

    Finish fleshing out image Film struct

--- src/camera/film.rs
+++ src/camera/film.rs
@@ -2,6 +2,28 @@ use camera::CameraSample;
 use filter::Filter;
 use spectrum::Spectrum;
 
+const FILTER_TABLE_DIM: usize = 16;
+const FILTER_TABLE_SIZE: usize = FILTER_TABLE_DIM * FILTER_TABLE_DIM;
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Pixel {
+    xyz: [f32; 3],
+    splat_xyz: [f32; 3],
+    weight_sum: f32,
+    _pad: f32
+}
+
+impl Pixel {
+    fn new() -> Pixel {
+        Pixel {
+            xyz: [0.0, 0.0, 0.0],
+            weight_sum: 0.0,
+            splat_xyz: [0.0, 0.0, 0.0],
+            _pad: 0.0
+        }
+    }
+}
+
 #[derive(Clone, Debug, PartialEq)]
 pub enum FilmTy {
     Image {
@@ -11,7 +33,9 @@ pub enum FilmTy {
         x_pixel_start: i32,
         y_pixel_start: i32,
         x_pixel_count: usize,
-        y_pixel_count: usize
+        y_pixel_count: usize,
+        pixels: Vec<Pixel>,  // !SPEED! Use a z-ordering here
+        filter_table: Vec<f32>
     }
 }
 
@@ -28,15 +52,22 @@ impl Film {
                  open_window: bool) -> Film {
         // Compute film image extent
         let x_start = ((xres as f32) * crop[0]).ceil() as i32;
-        let x_count = ::std::cmp::max((((xres as f32) * crop[1]).ceil() as i32) - x_start, 1);
+        let x_count = ::std::cmp::max((((xres as f32) * crop[1]).ceil() as i32) - x_start, 1) as usize;
         let y_start = ((yres as f32) * crop[2]).ceil() as i32;
-        let y_count = ::std::cmp::max((((yres as f32) * crop[3]).ceil() as i32) - y_start, 1);
+        let y_count = ::std::cmp::max((((yres as f32) * crop[3]).ceil() as i32) - y_start, 1) as usize;
 
         debug_assert!(x_count > 0);
         debug_assert!(y_count > 0);
 
-        // Allocate film image storage
         // Precompute filter weight table
+        let mut ft = vec![0.0; FILTER_TABLE_SIZE];
+        for y in 0..FILTER_TABLE_DIM {
+            let fy = ((y as f32) + 0.5) * filter.y_width() / (FILTER_TABLE_DIM as f32);
+            for x in 0..FILTER_TABLE_DIM {
+                let fx = ((x as f32) + 0.5) * filter.x_width() / (FILTER_TABLE_DIM as f32);
+                ft[y * FILTER_TABLE_DIM + x] = filter.evaluate(fx, fy);
+            }
+        }
 
         // Possibly open window for image display
         if open_window {
@@ -51,9 +82,14 @@ impl Film {
                 crop_window: crop,
                 filename: filename,
                 x_pixel_start: x_start,
-                x_pixel_count: x_count as usize,
+                x_pixel_count: x_count,
                 y_pixel_start: y_start,
-                y_pixel_count: y_count as usize
+                y_pixel_count: y_count,
+
+                // Allocate film image storage
+                pixels: vec![Pixel::new(); x_count * y_count],
+
+                filter_table: ft
             }
         }
     }
