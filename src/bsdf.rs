563762407858db5f2ab1de67f42d65c61625f455
Pavel Krajcevski <Krajcevski@gmail.com>
2016-03-31 23:25:43 -0400

    Add utility functions for calculating fresnel reflectance

--- src/bsdf.rs
+++ src/bsdf.rs
@@ -31,6 +31,34 @@ fn sin_phi(v: Vector) -> f32 {
     }
 }
 
+fn fr_diel(cosi: f32, cost: f32, etai: &Spectrum,
+           etat: &Spectrum) -> Spectrum {
+    let rparl =
+        ((etat * cosi) - (etai * cost)) /
+        ((etat * cosi) + (etai * cost));
+    let rperp =
+        ((etai * cosi) - (etat * cost)) /
+        ((etai * cosi) + (etat * cost));
+
+    (rparl * rparl + rperp * rperp) / 2.0
+}
+
+fn fr_cond(cosi: f32, eta: &Spectrum,
+           k: &Spectrum) -> Spectrum {
+    let tmp: Spectrum = (eta * eta + k * k) * cosi * cosi;
+    let rparl2 = {
+        let t: Spectrum = 2.0 * eta * cosi;
+        (tmp - t + 1.0) / (tmp + t + 1.0)
+    };
+
+    let tmp_f: Spectrum = eta*eta + k*k;
+    let rperp2 =
+        (tmp_f - (2.0 * eta * cosi) + cosi * cosi) /
+        (tmp_f + (2.0 * eta * cosi) + cosi * cosi);
+
+    (rparl2 + rperp2) / 2.0
+}
+
 bitflags! {
     pub flags BxDFType: u32 {
         const BSDF_REFLECTION = (1 << 0),
