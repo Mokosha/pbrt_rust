758b280536f825c2aca7165aa890fa44baa9e15e
Pavel Krajcevski <Krajcevski@gmail.com>
2016-04-02 23:37:23 -0400

    Make BSDF utils public

--- src/bsdf/utils.rs
+++ src/bsdf/utils.rs
@@ -3,12 +3,12 @@ use utils::Clamp;
 
 use std::f32;
 
-fn cos_theta(v: Vector) -> f32 { v.z }
-fn abs_cos_theta(v: Vector) -> f32 { v.z.abs() }
-fn sin_theta2(v: Vector) -> f32 { 0f32.max(1.0 - v.z*v.z) }
-fn sin_theta(v: Vector) -> f32 { sin_theta2(v).sqrt() }
+pub fn cos_theta(v: Vector) -> f32 { v.z }
+pub fn abs_cos_theta(v: Vector) -> f32 { v.z.abs() }
+pub fn sin_theta2(v: Vector) -> f32 { 0f32.max(1.0 - v.z*v.z) }
+pub fn sin_theta(v: Vector) -> f32 { sin_theta2(v).sqrt() }
 
-fn cos_phi(v: Vector) -> f32 {
+pub fn cos_phi(v: Vector) -> f32 {
     let vx = v.x;
     let sintheta = sin_theta(v);
     if sintheta == 0.0 {
@@ -18,7 +18,7 @@ fn cos_phi(v: Vector) -> f32 {
     }
 }
 
-fn sin_phi(v: Vector) -> f32 {
+pub fn sin_phi(v: Vector) -> f32 {
     let vy = v.y;
     let sintheta = sin_theta(v);
     if sintheta == 0.0 {
