60e72c9ac21100b8f30c0a2840dc144432fce743
Pavel Krajcevski <Krajcevski@gmail.com>
2016-04-20 23:52:19 -0400

    Texture parameters shouldn't be 'Copy'

--- src/texture.rs
+++ src/texture.rs
@@ -1,10 +1,10 @@
 use diff_geom::DifferentialGeometry;
 
 #[derive(Clone, Debug, PartialEq)]
-pub struct Texture<T: ?Sized+Copy> {
+pub struct Texture<T: ?Sized> {
     some_t: T
 }
 
-impl<T: ?Sized+Copy> Texture<T> {
+impl<T: ?Sized> Texture<T> {
     pub fn evaluate(&self, _: &DifferentialGeometry) -> T { self.some_t }
 }
