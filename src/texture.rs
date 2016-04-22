4ffd0d2217d69b1faccf655867a586334a642eaf
Pavel Krajcevski <Krajcevski@gmail.com>
2016-04-22 00:31:29 -0400

    Actually we need clone instead of copy

--- src/texture.rs
+++ src/texture.rs
@@ -1,10 +1,10 @@
 use diff_geom::DifferentialGeometry;
 
 #[derive(Clone, Debug, PartialEq)]
-pub struct Texture<T: ?Sized> {
+pub struct Texture<T: ?Sized + Clone> {
     some_t: T
 }
 
-impl<T: ?Sized> Texture<T> {
-    pub fn evaluate(&self, _: &DifferentialGeometry) -> T { self.some_t }
+impl<T: ?Sized + Clone> Texture<T> {
+    pub fn evaluate(&self, _: &DifferentialGeometry) -> T { self.some_t.clone() }
 }
