3fc3ca59fae847807cd9f3b61f24aa8c560ffa8c
Pavel Krajcevski <Krajcevski@gmail.com>
2016-04-25 23:44:37 -0400

    Add bump mapping, although I'm not happy with how cumbersome our vector code was to type check

--- src/material/mod.rs
+++ src/material/mod.rs
@@ -8,14 +8,62 @@ use std::sync::Arc;
 use bsdf::BSDF;
 use bsdf::bssrdf::BSSRDF;
 use diff_geom::DifferentialGeometry;
+use geometry::vector::*;
+use geometry::normal::*;
 use spectrum::Spectrum;
 use texture::Texture;
 
 use material::matte::MatteMaterial;
 
-pub fn bump(_: &Texture<f32>, _: &DifferentialGeometry,
-            _: &DifferentialGeometry) -> DifferentialGeometry {
-    unimplemented!()
+pub fn bump(d: &Texture<f32>, dg_geom: &DifferentialGeometry,
+            dg_shading: &DifferentialGeometry) -> DifferentialGeometry {
+    // Compute offset positions and evaluate displacement texture
+    let mut dg_eval = dg_shading.clone();
+
+    // Shift dg_eval.du in the u direction
+    let du = {
+        let mdu = 0.5 * (dg_shading.dudx.abs() + dg_shading.dudy.abs());
+        if mdu == 0.0 { 0.1 } else { mdu }
+    };
+
+    dg_eval.p = &dg_shading.p + du * &dg_shading.dpdu;
+    dg_eval.u = dg_shading.u + du;
+    dg_eval.nn = Normal::from(dg_shading.dpdu.clone().cross(&dg_shading.dpdv) + du * &dg_shading.dndu).normalize();
+
+    let u_displace = d.evaluate(&dg_eval);
+
+    // Shift dg_eval.dv in the v direction
+    let dv = {
+        let mdv = 0.5 * (dg_shading.dvdx.abs() + dg_shading.dvdy.abs());
+        if mdv == 0.0 { 0.1 } else { mdv }
+    };
+
+    dg_eval.p = &dg_shading.p + dv * &dg_shading.dpdv;
+    dg_eval.u = dg_shading.u;
+    dg_eval.v = dg_shading.v + dv;
+    dg_eval.nn = Normal::from(dg_shading.dpdu.clone().cross(&dg_shading.dpdv) + dv * &dg_shading.dndv).normalize();
+
+    let v_displace = d.evaluate(&dg_eval);
+    let displace = d.evaluate(dg_shading);
+    
+    // Compute bump mapped differential geometry
+    let mut dg_bump = dg_shading.clone();
+    dg_bump.dpdu = &dg_shading.dpdu
+        + (u_displace - displace) / du * Vector::from(dg_shading.nn.clone())
+        + displace * Vector::from(dg_shading.dndu.clone());
+    dg_bump.dpdv = &dg_shading.dpdv
+        + (v_displace - displace) / dv * Vector::from(dg_shading.nn.clone())
+        + displace * Vector::from(dg_shading.dndv.clone());
+    dg_bump.nn = Normal::from(dg_bump.dpdu.clone().cross(&dg_bump.dpdv).normalize());
+    if let Some(ref s) = dg_shading.shape {
+        if s.reverse_orientation ^ s.transform_swaps_handedness {
+            dg_bump.nn = Normal::new_with(-dg_bump.nn.x, -dg_bump.nn.y, -dg_bump.nn.z);
+        }
+    }
+
+    // Orient shading normal to match geometric normal
+    dg_bump.nn = dg_bump.nn.clone().face_forward(Vector::from(dg_geom.nn.clone()));
+    dg_bump
 }
 
 #[derive(Clone, PartialEq, Debug)]
