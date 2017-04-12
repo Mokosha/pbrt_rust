be36c69a896b4beab6c2dc8f9929365be0bc79f4
Pavel Krajcevski <Krajcevski@gmail.com>
2017-04-12 02:07:10 -0700

    Add UV mapping tests

--- src/texture/mapping.rs
+++ src/texture/mapping.rs
@@ -32,3 +32,50 @@ impl TextureMapping2D for UVMapping2D {
         (s, t, dsdx, dtdx, dsdy, dtdy)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use diff_geom::DifferentialGeometry;
+
+    #[test]
+    fn it_can_create_uv_mapping() {
+        let mapping = UVMapping2D::new(1.0, 1.0, 0.0, 0.0);
+        assert_eq!(mapping, UVMapping2D::new(1.0, 1.0, 0.0, 0.0));
+    }
+
+    #[test]
+    fn uv_mapping_can_map_coords() {
+        let identity = UVMapping2D::new(1.0, 1.0, 0.0, 0.0);
+        let mut dg = DifferentialGeometry::new();
+        assert_eq!(identity.map(&dg), (0.0, 0.0, 0.0, 0.0, 0.0, 0.0));
+
+        dg.u = 0.5;
+        dg.v = 0.2;
+        assert_eq!(identity.map(&dg), (0.5, 0.2, 0.0, 0.0, 0.0, 0.0));
+
+        dg.dudx = 10.0;
+        dg.dudy = 12.0;
+        dg.dvdx = -1.0;
+        dg.dvdy = 0.0;
+        assert_eq!(identity.map(&dg), (0.5, 0.2, 10.0, -1.0, 12.0, 0.0));
+    }
+
+    #[test]
+    fn uv_mapping_can_scale_coords() {
+        let mapping = UVMapping2D::new(2.0, 0.5, 1.0, -0.3);
+        let mut dg = DifferentialGeometry::new();
+        assert_eq!(mapping.map(&dg), (1.0, -0.3, 0.0, 0.0, 0.0, 0.0));
+
+        dg.u = 0.5;
+        dg.v = 0.2;
+        let expected_t = 0.2 * 0.5 - 0.3;  // Stupid floating point precision
+        assert_eq!(mapping.map(&dg), (2.0, expected_t, 0.0, 0.0, 0.0, 0.0));
+
+        dg.dudx = 10.0;
+        dg.dudy = 12.0;
+        dg.dvdx = -1.0;
+        dg.dvdy = 0.0;
+        assert_eq!(mapping.map(&dg), (2.0, expected_t, 20.0, -0.5, 24.0, 0.0));
+    }
+}
