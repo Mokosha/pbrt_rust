7a48e3586fb33222b8a8d44a8f55affb5804f3ba
Pavel Krajcevski <Krajcevski@gmail.com>
2017-08-11 00:30:35 -0700

    Formatting

--- src/primitive/mod.rs
+++ src/primitive/mod.rs
@@ -79,7 +79,8 @@ impl Primitive {
     pub fn geometric(s: Shape) -> Primitive {
         Primitive {
             base: PrimitiveBase::new(),
-            prim: Arc::new(Prim::Geometric(GeometricPrimitive::new(s, Material::broken())))
+            prim: Arc::new(Prim::Geometric(
+                GeometricPrimitive::new(s, Material::broken())))
         }
     }
 
