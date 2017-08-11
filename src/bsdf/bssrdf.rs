696e34e666219f99f1ead8f65d9f40a6ac52ca15
Pavel Krajcevski <Krajcevski@gmail.com>
2017-08-11 00:31:33 -0700

    Add subsurface material and BSSRDF

--- src/bsdf/bssrdf.rs
+++ src/bsdf/bssrdf.rs
@@ -1,2 +1,18 @@
-pub struct BSSRDF;
+use spectrum::Spectrum;
 
+#[derive(Clone, Debug, PartialEq)]
+pub struct BSSRDF {
+    eta: f32,
+    sig_a: Spectrum,
+    sigp_s: Spectrum
+}
+
+impl BSSRDF {
+    pub fn new(sa: Spectrum, sps: Spectrum, et: f32) -> BSSRDF {
+        BSSRDF { eta: et, sig_a: sa, sigp_s: sps }
+    }
+
+    pub fn eta(&self) -> f32 { self.eta }
+    pub fn sigma_a(&self) -> Spectrum { self.sig_a }
+    pub fn sigma_prime_s(&self) -> Spectrum { self.sigp_s }
+}
---
+++ src/material/subsurface.rs
@@ -0,0 +1,60 @@
+use std::sync::Arc;
+
+use bsdf::BSDF;
+use bsdf::bssrdf::BSSRDF;
+use bsdf::fresnel::Fresnel;
+use bsdf::specular::SpecularReflection;
+use diff_geom::DifferentialGeometry;
+use spectrum::Spectrum;
+use texture::Texture;
+use utils::Clamp;
+
+use material::bump;
+
+#[derive(Clone, Debug)]
+pub struct SubsurfaceMaterial {
+    scale: f32,
+    k_r: Arc<Texture<Spectrum>>,
+    sigma_a: Arc<Texture<Spectrum>>,
+    sigma_prime_s: Arc<Texture<Spectrum>>,
+    eta: Arc<Texture<f32>>,
+    bump_map: Option<Arc<Texture<f32>>>
+}
+
+impl SubsurfaceMaterial {
+    pub fn new(scale: f32, k_r: Arc<Texture<Spectrum>>,
+               sigma_a: Arc<Texture<Spectrum>>,
+               sigma_prime_s: Arc<Texture<Spectrum>>,
+               eta: Arc<Texture<f32>>,
+               bump_map: Option<Arc<Texture<f32>>>) -> SubsurfaceMaterial {
+        SubsurfaceMaterial { scale, k_r, sigma_a, sigma_prime_s, eta, bump_map }
+    }
+
+    pub fn get_bsdf(&self, dg_geom: DifferentialGeometry,
+                    dg_shading: DifferentialGeometry) -> Option<BSDF> {
+        let r = self.k_r.evaluate(&dg_geom).clamp(0.0, 1.0);
+        if r.is_black() {
+            return None;
+        }
+
+        // Allocate bsdf possibly doing bump mapping with bump map
+        let dgs = if let Some(ref tex) = self.bump_map {
+            bump(tex, &dg_geom, &dg_shading)
+        } else {
+            dg_shading
+        };
+
+        let fresnel = Fresnel::dielectric(1.0, self.eta.evaluate(&dg_geom));
+        let mut bsdf = BSDF::new(dgs.clone(), dg_geom.nn);
+        bsdf.add_bxdf(SpecularReflection::new(r, fresnel));
+        Some(bsdf)
+    }
+
+    pub fn get_bssrdf(&self, _: DifferentialGeometry,
+                      dgs: DifferentialGeometry) -> BSSRDF {
+        BSSRDF::new(self.scale * self.sigma_a.evaluate(&dgs),
+                    self.scale * self.sigma_prime_s.evaluate(&dgs),
+                    self.eta.evaluate(&dgs))
+    }
+
+}
